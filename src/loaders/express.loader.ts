0842f7967349f23eebe8c13a1de743d8655e6e95
agiratech-nagaraj <nagaraj@agiratech.com>
2020-03-16 20:42:11 +0530

    feat(Logger): Add morgon middleware logger
    new changes
    Integrated morgon logger streaming into wintson
    Handled Error and Listening of server

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -1,18 +1,34 @@
 import express from 'express';
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 import api from '../api/index';
+import {Container, Inject} from "typedi";
+import {Logger} from "winston";
+import LoggerInstance from "./loggers.loader";
 
 export class ExpressLoader{
 
     private constructor(
-        private app: express.Application
+        private app: express.Application,
     ) {
     }
 
     public config() {
+
+        const wintsonLogger: Logger = Container.get('LoggerInstance');
+        const stream = {
+            write: function(message: string) {
+                wintsonLogger.info(message);
+            },
+        };
+        const logger = morgan("combined" , {stream});
+
         this.app.use(bodyParser.json());
+        this.app.use(express.urlencoded({ extended: false }));
         this.app.use(bodyParser.urlencoded({extended: true}));
+        this.app.use(logger);
+
         api(this.app);
     }
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
-import http2 from 'http';
+import http2, {Server} from 'http';
 import "reflect-metadata";
 
 import loaders from './loaders'
 import config from './config';
+import {Container} from "typedi";
+import {Logger} from "winston";
 
 export class AppServer {
 
     public static PORT =  config.port || '3000';
+
 
     constructor() {
     }
@@ -15,11 +18,50 @@ export class AppServer {
     public static async  start(){
 
         const app = express();
-        console.log(config);
         const loadedModules = await loaders({ expressApp: app});
-        console.log(this.PORT);
-        http2.createServer(loadedModules.app).listen(this.PORT)
+        const server = http2.createServer(loadedModules.app)
+            .listen(this.PORT);
+        server.on( 'listening',  this.onListening(server));
+        server.on('error', this.onError(server))
 
+    }
+
+    public static onListening(server: Server){
+        return ()=>{
+            const addr = server.address();
+            console.log('listening');
+        }
+    }
+
+    public static onError(server: Server){
+        return (error: Error | any)=>{
+
+            const logger: Logger = Container.get('LoggerInstance');
+
+
+            if (error.syscall !== 'listen') {
+                throw error;
+            }
+
+            var bind = typeof this.PORT === 'string'
+                ? 'Pipe ' + this.PORT
+                : 'Port ' + this.PORT;
+
+            // handle specific listen errors with friendly messages
+            switch (error.code) {
+                case 'EACCES':
+                    logger.error(bind + ' requires elevated privileges');
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    logger.error(bind + ' is already in use');
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+
+        }
     }
 
 }
